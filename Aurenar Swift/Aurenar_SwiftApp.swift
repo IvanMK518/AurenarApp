608b0df5ea28453e9ab11be083fb274dc10fa296
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2025-06-10 16:01:22 -0500

    Initial Commit

---
+++ Aurenar Swift/Aurenar_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aurenar_SwiftApp.swift
+//  Aurenar Swift
+//
+//  Created by Ivan Martinez-Kay on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Aurenar_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aurenar SwiftTests/Aurenar_SwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  Aurenar_SwiftTests.swift
+//  Aurenar SwiftTests
+//
+//  Created by Ivan Martinez-Kay on 6/10/25.
+//
+
+import Testing
+@testable import Aurenar_Swift
+
+struct Aurenar_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
